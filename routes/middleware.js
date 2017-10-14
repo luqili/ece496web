f9b1c0fb1756de48e5f865e7e69a6fa3745e5a94
luqili <hzluqili@gmail.com>
2017-10-13 22:48:30 -0400

    change gallery to demo
    rt

--- routes/middleware.js
+++ routes/middleware.js
@@ -21,7 +21,7 @@ exports.initLocals = function (req, res, next) {
 	res.locals.navLinks = [
 		{ label: 'Home', key: 'home', href: '/' },
 		{ label: 'Update', key: 'blog', href: '/blog' },
-		{ label: 'Gallery', key: 'gallery', href: '/gallery' },
+		{ label: 'Demo', key: 'gallery', href: '/gallery' },
 		{ label: 'Contact', key: 'contact', href: '/contact' },
 	];
 	res.locals.user = req.user;
